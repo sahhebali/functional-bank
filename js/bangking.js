a6a023b00099e7fa59ed9485df006c8a68bb881a
sahhebali <saheb.co21@gmail.com>
2021-09-14 23:04:56 +0530

    onn-functionadd

--- js/bangking.js
+++ js/bangking.js
@@ -1,39 +1,38 @@
 document.getElementById('diposit-button').addEventListener('click', function () {
-    const depositInput = document.getElementById('diposit-input');
-    const depositAmountText = depositInput.value
-    const depositAmount = parseFloat(depositAmountText);
-    // console.log(depositAmount);
-    const dipositTotal = document.getElementById('diposit-total');
-    const depositTotalText = dipositTotal.innerText;
-    const depositAmountTotal = parseFloat(depositTotalText);
-    dipositTotal.innerText = depositAmount + depositAmountTotal;
-
-    const balanceTotal = document.getElementById('blance-total')
-    const balanceTotalText = balanceTotal.innerText;
-    const balanceAmount = parseFloat(balanceTotalText);
-    // console.log(balanceAmount);
-    balanceTotal.innerText = balanceAmount + depositAmount;
-
-
+    changBalance("deposit");
 })
 
-
+// onfunction rulse
 document.getElementById('withdraw-button').addEventListener('click', function () {
-    const withDrawButton = document.getElementById('withdraw-input');
-    const withDrawButtonText = withDrawButton.value;
-    const withdrawAmount = parseFloat(withDrawButtonText);
-    // console.log(withdrawAmount);
-    // console.log('check withdraw button');
-
-    const withdrawTotal = document.getElementById('withdraw-total');
-    const withdrawText = withdrawTotal.innerText;
-    const withdrawTotalAmount = parseFloat(withdrawText);
-    console.log(withdrawTotalAmount);
-    withdrawTotal.innerText = withdrawTotalAmount + withdrawAmount;
-
-    const balanceTotal = document.getElementById('blance-total');
-    const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(balanceTotalText);
-    balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
-
+    changBalance("withdraw");
 })
+
+
+function changBalance(type) {
+
+    const previousTotal = document.getElementById(`${type}-total`);
+    const previousTotalText = previousTotal.innerText;
+    const previousTotalNumber = parseFloat(previousTotalText);
+
+    const newMoney = document.getElementById(`${type}-input`);
+    const newMoneyText = newMoney.value
+    if (newMoneyText.length != 0) {
+        const newmoneyNumber = parseFloat(newMoneyText);
+
+        previousTotal.innerText = previousTotalNumber + newmoneyNumber;
+        // console.log(depositAmount);
+        const balanceTotal = document.getElementById('blance-total')
+        const balanceTotalText = balanceTotal.innerText;
+        const balanceTotalNumber = parseFloat(balanceTotalText);
+        if (type == "withdraw") {
+            balanceTotal.innerText = balanceTotalNumber - newmoneyNumber;
+        }
+        else {
+            balanceTotal.innerText = balanceTotalNumber + newmoneyNumber;
+        }
+    }
+    else {
+        alert("please enter a number");
+    }
+
+}
